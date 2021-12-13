6771d6db03cca3c4ab365ae65e743eb36bbfda4e
awesomelewis2007 <60709927+awesomelewis2007@users.noreply.github.com>
2021-12-13 20:59:09 +0000

    Add files via upload

---
+++ Rover/Info.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Reflection;
+namespace Rover
+{
+	/// <summary>
+	/// Interaction logic for Info.xaml
+	/// </summary>
+	public partial class Info : Window
+	{
+		public Info()
+		{
+			InitializeComponent();
+			Version.Content = Assembly.GetEntryAssembly().GetName().Version.ToString();
+		}
+
+		private void Close_click(object sender, RoutedEventArgs e)
+		{
+			Close();
+		}
+		private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+		{
+			if (e.ChangedButton == MouseButton.Left)
+				this.DragMove();
+		}
+	}
+}
---
+++ Rover/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Rover
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+
+		}
+		private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+		{
+			if (e.ChangedButton == MouseButton.Left)
+				this.DragMove();
+		}
+		private void Window_Deactivated(object sender, EventArgs e)
+		{
+			Window window = (Window)sender;
+			window.Topmost = true;
+		}
+
+		private void Close_Click(object sender, RoutedEventArgs e)
+		{
+			Close();
+		}
+		private void Info_Click(object sender, RoutedEventArgs e)
+		{
+			Info info = new Info();
+			info.Show();
+		}
+	}
+}
